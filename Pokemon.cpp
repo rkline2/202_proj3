1df8bc18e1c32349abbaff53d606ffafe17feecd
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-03-31 17:28:43 -0400

    Pokemon.cpp Update 3.31.20

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -6,69 +6,85 @@ using namespace std;
     // Desc - Default Constructor
     // Preconditions - None
     // Postconditions - Creates Pokemon with default values
-Pokemon::Pokemon(){}
+Pokemon::Pokemon(){
+    m_index = 0;
+    m_health = 0;
+    m_name = "";
+    m_type = "";
+    m_strong = "";
+}
 
 // Name: Pokemon(int, string, string, string)
     // Desc - Overloaded Constructor
     // Preconditions - Have index, name, type, and what the Pokemon is strong against
     // Postconditions - Creates Pokemon using passed values
-Pokemon::Pokemon(int, string, string, string) {}
+Pokemon::Pokemon(int index, string name, string type, string strength) {
+    m_index = index; 
+    m_name = name;
+    m_type = type;
+    m_strong = strength;
+    m_health = MAX_HEALTH;
+}
 
 // Name: GetNext
     // Desc - Returns pointer to the next pokemon
     // Preconditions - Pokemon exists
     // Postconditions - Returns pointer to next pokemon in linked list
-//Pokemon* Pokemon::GetNext(){}
+// may be issues here not sure 
+Pokemon* Pokemon::GetNext() { return m_next; }
 
 // Name: GetName
     // Desc - Returns Name
     // Preconditions - Pokemon exists
     // Postconditions - Returns name of Pokemon
-//string Pokemon::GetName(){}
+string Pokemon::GetName() { return m_name; }
 
 // Name: GetIndex
     // Desc - Returns index
     // Preconditions - Pokemon exists
     // Postconditions - Returns index of Pokemon
-//int Pokemon::GetIndex(){}
+int Pokemon::GetIndex() { return m_index; }
 
 // Name: GetType
     // Desc - Returns type
     // Preconditions - Pokemon exists
     // Postconditions - Returns type of Pokemon
-//string Pokemon::GetType(){}
+string Pokemon::GetType() { return m_type; }
 
 // Name: GetHealth
     // Desc - Returns health
     // Preconditions - Pokemon exists
     // Postconditions - Returns health of Pokemon
-//int Pokemon::GetHealth(){}
+int Pokemon::GetHealth() { return m_health; }
 
 // Name: GetStrong
     // Desc - Returns type of Pokemon this Pokemon is strong against
     // Preconditions - Pokemon exists
     // Postconditions - Returns type of Pokemon this Pokemon is strong against
-//string Pokemon::GetStrong(){}
+string Pokemon::GetStrong() { return m_strong; }
 
 // Name: SetHealth
     // Desc - Sets health of pokemon
     // Preconditions - Pokemon exists
     // Postconditions - Updates health of Pokemon
-void Pokemon::SetHealth(int){}
+void Pokemon::SetHealth(int newHealth) { m_health = newHealth; }
 
 // Name: SetNext
     // Desc - Sets the pointer to the next Pokemon in the list
     // Preconditions - Pokemon exists
     // Postconditions - Sets the pointer to the next Pokemon
-void Pokemon::SetNext(Pokemon*){}
+void Pokemon::SetNext(Pokemon* current) { m_next = current; }
 
 // Name: Overloaded << Operator
     // Desc - Allows us to cout a Pokemon object
     // Preconditions - Pokemon exists
     // Postconditions - When a Pokemon object is cout, shows name, health, and type
 
-// not sure if this is the right format, will check later
-//ostream& operator<<(ostream& output, Pokemon& P) {}
+
+ostream& operator<<(ostream& output, Pokemon& P) {
+    return output << P.GetName() << " (" << P.GetType() 
+        << ": " << P.GetHealth() << " health)";
+}
 
 /* string m_name; //Name of Pokemon
     int m_index; //Index of Pokemon
